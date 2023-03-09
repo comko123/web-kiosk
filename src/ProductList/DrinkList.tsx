1fb0b90c12da66e3d0e29e103ba4a69e581ce2a0
Minseok Kim <comko123@naver.com>
2023-03-09 19:12:03 +0900

    css modifire

--- src/ProductList/DrinkList.tsx
+++ src/ProductList/DrinkList.tsx
@@ -22,7 +22,7 @@ const DrinkList = ({locationProducts,link}:IDrinkListProps) => {
     if(defaultOptionC!==-1)setOptionState("Ice")
     else if(defaultOptionH!==-1)setOptionState("Hot")},[searchparms.get("name")])
     return(<><ul className="list-group products">
-    {locationProducts.map(v =><li key={v.productId} className="list-group-item d-flex mt-3" id="listBox">
+    {locationProducts.map(v =><li key={v.productId} className="list-group-item d-flex" id="listBox">
     <Product {...v} /></li>)}</ul><AnimatePresence>{match?
     <><motion.div id="detailOutBox" onClick={()=>navigate(-1)} animate={{opacity:1}} exit={{opacity:0}}/>
     <motion.div className="detailInBox" layoutId={searchparms.get("name")+""}>
