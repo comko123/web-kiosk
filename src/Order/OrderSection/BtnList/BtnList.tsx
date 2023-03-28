f63f139afa0257457408d8e882e2486d079b68a5
Minseok Kim <comko123@naver.com>
2023-03-29 00:48:20 +0900

    code factor

--- src/Order/OrderSection/BtnList/BtnList.tsx
+++ src/Order/OrderSection/BtnList/BtnList.tsx
@@ -2,5 +2,10 @@ import VTbutton from "./VTbutton"
 
 const BtnList = () => {
 return(
-<>{[{k:"방문",e:"STORE"},{k:"포장",e:"TAKEOUT"}].map(({k,e})=><VTbutton key={k} vt={k} receiveType={e as "STORE"|"TAKEOUT"}/>)}</>)}
+<>{[{k:"방문",e:"STORE"},{k:"포장",e:"TAKEOUT"}].map(({k,e})=>{return(
+<VTbutton 
+vt={k} 
+key={k} 
+receiveType={e as "STORE"|"TAKEOUT"}
+/>)})}</>)}
 export default BtnList
