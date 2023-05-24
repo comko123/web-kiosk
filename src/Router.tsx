577402eeefc81d796b91371af0e75be211c49aea
Minseok Kim <comko123@naver.com>
2023-05-25 00:58:08 +0900

    add : error page added

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,18 +2,24 @@ import App from "./App"
 import { createBrowserRouter } from "react-router-dom"
 import { lazy, Suspense } from "react"
 import FallbackPage from "@Fallback"
+import ErrorElement from "@Error"
 const MainIndex = lazy(() => import("@MainIndex"))
 const AllDrink = lazy(() => import("@AllDrink"))
 const CategoryDrink = lazy(() => import("@CategoryDrink"))
 const Router = createBrowserRouter([{
-    path: "/", element: <App />, children: [
+    path: "/",errorElement:<ErrorElement/>,
+     element: <App />, children: [
         {
-            path: "", element: <Suspense fallback={<FallbackPage/>}><MainIndex/></Suspense>, children: [{
-                path: "productName", element: <Suspense fallback={<FallbackPage/>}><AllDrink /></Suspense>
+            path: "", errorElement:<ErrorElement/>,
+            element: <Suspense fallback={<FallbackPage/>}><MainIndex/></Suspense>, children: [{
+                path: "productName", 
+                element: <Suspense fallback={<FallbackPage/>}><AllDrink /></Suspense>
             },
             {
-                path: ":drink", element: <Suspense fallback={<FallbackPage/>}><CategoryDrink /></Suspense>, children: [{
-                    path: "productName", element: <Suspense fallback={<FallbackPage/>}><CategoryDrink /></Suspense>
+                path: ":drink",errorElement:<ErrorElement/>, 
+                element: <Suspense fallback={<FallbackPage/>}><CategoryDrink /></Suspense>, children: [{
+                    path: "productName", errorElement:<ErrorElement/>,
+                    element: <Suspense fallback={<FallbackPage/>}><CategoryDrink /></Suspense>
                 }]
             }]
         }]
