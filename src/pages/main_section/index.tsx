335f2e9ac057b60d88540041f5033cf733b18449
Minseok Kim <comko123@naver.com>
2023-06-19 00:03:12 +0900

    lazy load

--- src/pages/main_section/index.tsx
+++ src/pages/main_section/index.tsx
@@ -9,7 +9,7 @@ const Main = () => {
     return (<><div className="container-fluid">
         <div className="row justify-content-center m-4">
             <div style={{ textAlign: "center" }}><Link to="/" className='mainText'>
-                <img src="https://image.istarbucks.co.kr/common/img/common/favicon.ico?v=200828" alt='' />
+                <img src="https://image.istarbucks.co.kr/common/img/common/favicon.ico?v=200828" loading='lazy' alt='' />
             </Link></div></div><div className="card"><div className="row">
                 <div className="col-md-8 d-flex flex-column align-items-start padding-option pt-0">
                     <ProductList /></div><div className="col-md-4 summary p-4">
